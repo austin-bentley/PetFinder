64c6b544657dc74f0573cf2903fbe6b0408cf4b7
abentley <abentley-contractor@cars.com>
2019-08-06 15:34:38 -0600

    seperate context

--- lib/petFinder_web/controllers/page_controller.ex
+++ lib/petFinder_web/controllers/page_controller.ex
@@ -1,6 +1,7 @@
 defmodule PetFinderWeb.PageController do
   use PetFinderWeb, :controller
   alias PetFinder.Pet
+  alias PetFinder.Location
 
   def index(conn, _params) do
     user_id = Plug.Conn.get_session(conn, :current_user_id)
@@ -10,7 +11,7 @@ defmodule PetFinderWeb.PageController do
       user_id: user_id,
       recently_lost_animals: Pet.get_recently_lost_animals!(),
       recently_found_animals: Pet.get_recently_found_animals!(),
-      posts_near_user: Pet.get_posts_near_user(user_id)
+      posts_near_user: Location.get_posts_near_user(user_id)
     )
   end
 end
